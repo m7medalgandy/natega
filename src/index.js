0caaa615ce6ab381d6a990e30dfab74a8bb62f0e
m7medalgandy <142119759+m7medalgandy@users.noreply.github.com>
2024-08-06 00:50:50 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
 const R2_BUCKET_NAME = 'ntega';
-const EXCEL_FILE_NAME = 'نتيجة الثانوية 24.xlsx'; // اسم ملف Excel الخاص بك
+const EXCEL_FILE_NAME = 'students-results.xlsx'; // اسم ملف Excel الخاص بك
